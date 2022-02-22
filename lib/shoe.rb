0b0f6c1200e0b174ee8fa213f4d3ae406b4fa2ef
Layna Oyervides <laynaoyervides@gmail.com>
2022-02-21 21:24:10 -0700

    commit tree bf7c46bcd102880ad90e908cc449b9dad158e847

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe 
+    attr_accessor :condition, :color, :size, :material
+    attr_reader :brand
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"        
+    end
+    def initialize(brand)
+        @brand = brand
+    end
+end
